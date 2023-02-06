7c53c0d1a76a0516bff470393fa8116a26a41eac
Stepan <stepa.davydov.2002@mail.ru>
2023-02-06 17:09:23 +0300

    Code has been cleared

--- main.go
+++ main.go
@@ -18,13 +18,14 @@ var (
 	username          string
 	masterPassword    string
 	site              string
-	login             bool   = false
-	encVariants              = []string{"Maximum", "Long", "Medium", "Basic", "Short", "PIN", "Name", "Phrase"}
-	encSelected       int32  = 0
-	passNum           int32  = 1
-	generatedPassword string = ""
-	auto              bool   = false
+	login                   = false
+	encVariants             = []string{"Maximum", "Long", "Medium", "Basic", "Short", "PIN", "Name", "Phrase"}
+	encSelected       int32 = 0
+	passNum           int32 = 1
+	generatedPassword       = ""
+	auto                    = false
 )
+
 var characters = map[byte]string{
 	'V': "AEIOU",
 	'C': "BCDFGHJKLMNPQRSTVWXYZ",
@@ -96,6 +97,7 @@ var iconData []byte
 var icons []image.Image
 
 func generateUniqueKey() string {
+	// Generate a unique string from the username, master password, site and password number
 	seed := hmac.New(sha256.New, []byte(masterPassword+strconv.Itoa(len(site))+site+strconv.Itoa(int(passNum))))
 	salt := []byte(site + string(rune(len(username))) + username + hex.EncodeToString(seed.Sum(nil)))
 	dk, _ := scrypt.Key([]byte(masterPassword), salt, 32768, 8, 2, 64)
@@ -106,14 +108,15 @@ func generateUniqueKey() string {
 func generatePassword() {
 	originalString := generateUniqueKey()
 	result := ""
-	lettersCode := []int{}
-
+	var lettersCode []int
+	// Convert the string to a slice of integers
 	for _, r := range originalString {
 		lettersCode = append(lettersCode, int(r))
 	}
 	templateType := "template" + encVariants[encSelected]
 	resultTemplate := templates[templateType][lettersCode[0]%len(templates[templateType])]
 	i := 0
+	// Generate the password using the template
 	for _, characterClass := range resultTemplate {
 		characters := characters[byte(characterClass)]
 		result += string(characters[lettersCode[i+1]%len(characters)])
@@ -123,6 +126,7 @@ func generatePassword() {
 
 }
 
+// Auto-generate password when the user changes the settings
 func autoGeneratePassword() {
 	if auto {
 		generatePassword()
@@ -134,16 +138,13 @@ func loop() {
 			if !login {
 				giu.PopupModal("Login:##" + strconv.Itoa(1)).Flags(6).Layout(
 					giu.Custom(func() {
-						const buttonW, buttonH = -1, 30
-						//_, availableH := giu.GetAvailableRegion()
-						//_, itemSpacingY := giu.GetItemSpacing()
 						giu.Layout{
 							giu.Dummy(180, 0),
 							giu.Label("Username:"),
 							giu.InputText(&username).Size(-1),
 							giu.Label("Master password:"),
 							giu.InputText(&masterPassword).Size(-1).Flags(giu.InputTextFlagsPassword),
-							giu.Button("Login").Size(buttonW, buttonH).OnClick(func() {
+							giu.Button("Login").Size(-1, 30).OnClick(func() {
 								if (username == "") || (masterPassword == "") {
 									return
 								}
@@ -156,57 +157,54 @@ func loop() {
 				giu.Update()
 			}
 		}),
-		giu.Custom(func() { // TODO delete Custom
-			giu.Layout{
-				giu.Label("Generate password for " + username),
-				giu.Separator(),
+		giu.Layout{
+			giu.Label("Generate password for " + username),
+			giu.Separator(),
+			giu.Row(
+				giu.Label("Site name:"),
+				giu.InputText(&site).Size(-1).OnChange(autoGeneratePassword),
+			),
+			giu.Style().SetDisabled(site == "").To(
 				giu.Row(
-					giu.Label("Site name:"),
-					giu.InputText(&site).Size(-1).OnChange(autoGeneratePassword),
+					giu.Combo("##splitter", encVariants[encSelected], encVariants, &encSelected).Size(-1).OnChange(autoGeneratePassword),
+					giu.Tooltip("Type of password to generate"),
+
+					giu.InputInt(&passNum).Size(-1).Flags(giu.InputTextFlagsCharsDecimal).OnChange(autoGeneratePassword),
+					giu.Tooltip("Number of password"),
 				),
-				giu.Style().SetDisabled(site == "").To(
-					giu.Row(
-						giu.Combo("##splitter", encVariants[encSelected], encVariants, &encSelected).Size(-1).OnChange(autoGeneratePassword),
-						giu.Tooltip("Type of password to generate"),
 
-						giu.InputInt(&passNum).Size(-1).Flags(giu.InputTextFlagsCharsDecimal).OnChange(autoGeneratePassword),
-						giu.Tooltip("Number of password"),
-					),
-
-					giu.Row(
-						giu.Checkbox("", &auto),
-						giu.Tooltip("Auto generate password"),
-						giu.Button("Generate").Size(-1, 0).OnClick(generatePassword),
-					),
-					giu.Style().SetDisabled(generatedPassword == "").To(
-						giu.Dummy(0, 10),
-						giu.Align(giu.AlignCenter).To(
-							giu.Label(generatedPassword),
-						),
-						giu.Dummy(0, 10),
-						giu.Button("copy").Size(-1, 0).OnClick(func() {
-							giu.Context.GetPlatform().SetClipboard(generatedPassword)
-
-						}),
-					),
+				giu.Row(
+					giu.Checkbox("", &auto),
+					giu.Tooltip("Auto generate password"),
+					giu.Button("Generate").Size(-1, 0).OnClick(generatePassword),
 				),
-			}.Build()
-		}),
+				giu.Style().SetDisabled(generatedPassword == "").To(
+					giu.Dummy(0, 10),
+					giu.Align(giu.AlignCenter).To(
+						giu.Label(generatedPassword),
+					),
+					giu.Dummy(0, 10),
+					giu.Button("copy").Size(-1, 0).OnClick(func() {
+						giu.Context.GetPlatform().SetClipboard(generatedPassword)
+
+					}),
+				),
+			),
+		},
 	)
 }
 
 func main() {
 	wnd := giu.NewMasterWindow("Master Password", 318, 180, giu.MasterWindowFlagsNotResizable)
+	// Set the icon of the window
 	icons = make([]image.Image, 0)
-
 	icon1, _, err := image.Decode(bytes.NewReader(iconData))
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	icons = append(icons, icon1)
-
 	wnd.SetIcon(icons)
+
 	wnd.Run(loop)
 
 }
